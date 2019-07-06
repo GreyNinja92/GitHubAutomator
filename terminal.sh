0b8a47edab959e7da06e40c3659bd10fabf05610
GreyNinja92 <50029537+GreyNinja92@users.noreply.github.com>
2019-07-07 00:25:52 +0530

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -1,5 +1,5 @@
 cd
-path=/Users/'your_home_directory'/Documents/Projects/
+path=/Users/<your_home_directory>/Documents/Projects/
 cd Desktop/Automate/
 python Automate.py $1
 cd $path
@@ -7,8 +7,7 @@ mkdir $1
 cd $1
 touch README.md
 git init
-git remote add origin git@github.com:GreyNinja92/$1.git
+git remote add origin git@github.com:<your_GitHub_Username>/$1.git
 git add .
 git commit -m "Initial commit"
 git push -u origin master
-
